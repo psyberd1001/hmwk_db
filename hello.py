8e49325433f2915125ced3b626e76f602c9650c2
psyberd1001 <108468415+psyberd1001@users.noreply.github.com>
2024-12-11 08:28:08 +0300

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,36 @@
+import random
+import sqlite3
+
+connection = sqlite3.connect("database3.db")
+cursor = connection.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Users(
+id INTEGER PRIMARY KEY,
+username TEXT NOT NULL,
+email TEXT NOT NULL,
+age INTEGER,
+balance INTEGER NOT NULL
+)
+''')
+
+cursor.execute("CREATE INDEX IF NOT EXISTS idx_email ON Users (email)")
+
+# for i in range(1, 11):
+#    cursor.execute("INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)",
+#               (f"newuser{i}", f"{i}ex@gmail.com", f"{i*10}", f"{1000}"))
+# for i in range(1, 11):
+#     if i % 2 != 0:
+#         cursor.execute("UPDATE Users SET balance = ? WHERE username = ?", (500, f"newuser{i}"))
+
+# for i in range(1, 11, 3):
+#     cursor.execute("DELETE FROM Users WHERE username = ?", (f"newuser{i}",))
+
+cursor.execute("SELECT username, email, age, balance FROM Users GROUP BY AGE")
+users = cursor.fetchall()
+for user in users:
+    if user[2] != 60:
+        print(f"Имя: {user[0]} | Почта: {user[1]} | Возраст: {user[2]} | Баланс: {user[3]}")
+
+connection.commit()
+connection.close()
